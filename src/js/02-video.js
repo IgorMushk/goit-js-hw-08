8817deea7c3a34dff31ac583d216c8f9e153973e
IgorMushk <igor.mushketik@gmail.com>
2023-07-13 18:58:11 +0300

    Exercise-02 - finish - comments removed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,35 +2,16 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
+const KEY_STORAGE = 'videoplayer-current-time';
 const player = new Player(iframe);
 
-//localStorage.clear();
-
-const starTimePlay = localStorage.getItem('videoplayer-current-time');
+const starTimePlay = localStorage.getItem(KEY_STORAGE);
 if (starTimePlay) {
-  console.log(Number(starTimePlay));
   player.setCurrentTime(starTimePlay);
 }
 
 const onPlay = function (data) {
-  // data is an object containing properties specific to that event
-  console.log('played the video!');
-  //console.log(data);
-  //localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
-  localStorage.setItem('videoplayer-current-time', data.seconds);
-  console.log(data.seconds);
+  localStorage.setItem(KEY_STORAGE, data.seconds);
 };
 
-//const throttleHandler = _.throttle(onPlay, 1000);
-//player.on('timeupdate', onPlay);
 player.on('timeupdate', throttle(onPlay, 1000));
-//player.on('timeupdate', throttleHandler);
-
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
