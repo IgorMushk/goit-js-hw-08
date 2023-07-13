d3b117a25fb8e481caae2ff40445c90921793aa7
IgorMushk <igor.mushketik@gmail.com>
2023-07-13 12:46:11 +0300

    Add  list-style: none;

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,44 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesListArr = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+});
+
+const galleryList = document.querySelector('.gallery');
+galleryList.insertAdjacentHTML('beforeend', imagesListArr.join(''));
+
+galleryList.addEventListener('click', handleImgClick);
+
+function handleImgClick(event) {
+  if (!event.target.classList.contains('gallery__image')) return;
+  event.preventDefault();
+  const urlImgLarge = event.target.dataset.source;
+  showImageModal(urlImgLarge);
+}
+
+function showImageModal(urlImg) {
+  const instance = basicLightbox.create(`
+    <img src="${urlImg}" width="800" height="600">
+`);
+  instance.show();
+
+  document.addEventListener('keydown', checkClose);
+
+  function checkClose(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      document.removeEventListener('keydown', checkClose);
+    }
+  }
+}
