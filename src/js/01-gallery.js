07eeca403e63aa77b08f061d8f3ba5c31c454e4b
IgorMushk <igor.mushketik@gmail.com>
2023-07-13 14:22:40 +0300

    Exercise 01 finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,11 +8,10 @@ console.log(galleryItems);
 
 const imagesListArr = galleryItems.map(({ preview, original, description }) => {
   return `<li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="${original}"
       alt="${description}"
     />
   </a>
@@ -22,27 +21,7 @@ const imagesListArr = galleryItems.map(({ preview, original, description }) => {
 const galleryList = document.querySelector('.gallery');
 galleryList.insertAdjacentHTML('beforeend', imagesListArr.join(''));
 
-//galleryList.addEventListener('click', handleImgClick);
-
-// function handleImgClick(event) {
-//   if (!event.target.classList.contains('gallery__image')) return;
-//   event.preventDefault();
-//   const urlImgLarge = event.target.dataset.source;
-//   showImageModal(urlImgLarge);
-// }
-
-// function showImageModal(urlImg) {
-//   const instance = basicLightbox.create(`
-//     <img src="${urlImg}" width="800" height="600">
-// `);
-//   instance.show();
-
-//   document.addEventListener('keydown', checkClose);
-
-//   function checkClose(event) {
-//     if (event.code === 'Escape') {
-//       instance.close();
-//       document.removeEventListener('keydown', checkClose);
-//     }
-//   }
-// }
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
