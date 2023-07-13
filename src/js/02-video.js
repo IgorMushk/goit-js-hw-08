72e8ea8a6078384c69182dbb7c9c2a2e86614e1d
IgorMushk <igor.mushketik@gmail.com>
2023-07-13 17:23:24 +0300

    Exercise-02 -Read timePlayer from ocalStorage &  setCurrentTime

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,21 @@ const iframe = document.querySelector('iframe');
 // const player = new Vimeo.Player(iframe);
 const player = new Player(iframe);
 
+//localStorage.clear();
+
+const starTimePlay = localStorage.getItem('videoplayer-current-time');
+if (starTimePlay) {
+  console.log(Number(starTimePlay));
+  player.setCurrentTime(starTimePlay);
+}
+
 const onPlay = function (data) {
   // data is an object containing properties specific to that event
   console.log('played the video!');
-  console.log(data);
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  //console.log(data);
+  //localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  console.log(data.seconds);
 };
 
 player.on('timeupdate', onPlay);
