b4ab766940de5d13feebe5acdaf6f6729d25188e
IgorMushk <igor.mushketik@gmail.com>
2023-07-13 15:22:01 +0300

    Exercise listen for events in the playe

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,9 +4,15 @@ const iframe = document.querySelector('iframe');
 // const player = new Vimeo.Player(iframe);
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
